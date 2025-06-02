efbd49b199a7acf39e92b4021300fb3967c04b4b
mingoo <chemd8@gmail.com>
2025-06-02 12:40:09 +0900

    sovle HW01

---
+++ HW01/HW01.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void sumarray(int array[], int size) {
+	int sum = 0;
+	for (int s = 0; s < size; s++) {
+		sum += array[s];
+	}
+	cout << "합계 : " << sum << endl;
+}
+
+void avgarray(int array[], int size) {
+	int avg = 0;
+	int sum = 0;
+	for (int s = 0; s < size; s++) {
+		sum += array[s];
+	}
+	avg = sum / 5;
+	cout << "평균 : " << avg << endl;
+}
+
+int main()
+{
+	int arr[5];
+	for (int i = 0; i < 5; ++i) {
+		cout << "입력 : ";
+		cin >> arr[i];
+	}
+	sumarray(arr, 5);
+	avgarray(arr, 5);
+	return 0;
+}
---
+++ HW01Challenge/HW01Challenge.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int Num[5];
+	int input;
+	for (int a = 0; a < 5; a++) {
+		cout << "숫자를 입력하시오 : ";
+		cin >> Num[a];
+	}
+	cout << "1(오름차순) 혹은 2(내림차순)를 입력하시오 : ";
+	cin >> input;
+	if (input == 1) {
+		for (int a = 0; a < 4; a++) {
+			for (int b = 0; b < 4 - a; b++)
+				if (Num[b] > Num[b + 1]) {
+					swap(Num[b], Num[b + 1]);
+				}
+		}
+		for (int a : Num) {
+			cout << a << " ";
+		}
+	}
+	else if (input == 2) {
+		for (int a = 0; a < 4; a++) {
+			for (int b = 0; b < 4 - a; b++)
+				if (Num[b] < Num[b + 1]) {
+					swap(Num[b], Num[b + 1]);
+				}
+		}
+		for (int a : Num) {
+			cout << a << " ";
+		}
+	}
+	else {
+		cout << "입력 오류";
+	}
+	return 0;
+}
